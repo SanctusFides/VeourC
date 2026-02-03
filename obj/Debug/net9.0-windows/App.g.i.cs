809f063c1bfbbcc96bcbe6603361d6bdb7ecd003
Sanctus <jhn.hines@gmail.com>
2026-02-03 13:54:02 -0600

    Cleaned up the old EXE files that were polluting the folder

--- obj/Debug/net9.0-windows/App.g.i.cs
+++ obj/Debug/net9.0-windows/App.g.i.cs
@@ -50,7 +50,7 @@ namespace Veour {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -76,7 +76,7 @@ namespace Veour {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.2.0")]
         public static void Main() {
             Veour.App app = new Veour.App();
             app.InitializeComponent();
